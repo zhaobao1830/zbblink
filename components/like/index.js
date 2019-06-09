d227d35973ca96909b6c972f80e532ef07955e3e
zhaobao1830 <373098777@qq.com>
2019-06-09 15:47:08 +0800

    like组件完成

--- components/like/index.js
+++ components/like/index.js
@@ -1,66 +1,39 @@
 // components/like/index.js
-Page({
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    like: {
+      type: Boolean,
+      default: false
+    },
+    count: {
+      type: Number
+    }
+  },
 
   /**
-   * 页面的初始数据
+   * 组件的初始数据
    */
   data: {
-
+    yesSrc: 'images/like.png',
+    noSrc: 'images/like@dis.png'
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 组件的方法列表
    */
-  onLoad: function (options) {
+  methods: {
+    onLike:function (event) {
+      let like = this.properties.like
+      let count = this.properties.count
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      count = like?count-1:count+1
+      this.setData({
+        count:count,
+        like:!like
+      })
+    }
   }
 })
